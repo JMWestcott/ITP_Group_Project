8a9fe0e828c62bdca1686703c37c0a9db7ceda52
Shaleigh Smith <sha@Shaleighs-MacBook-Pro.local>
2018-08-06 17:38:21 -0400

    Commit 6/8 update

--- SHA_ITP_ttt_test.py
+++ SHA_ITP_ttt_test.py
@@ -61,134 +61,141 @@ def tic_tac_toe():
         #sum(map(sum, grid)) < ((N*N/2)*3)-1
         # if sum(map(sum, grid)) < ((N*N/2)*3):
         #if (N%2 == 0 and i <= (N*N)/2) or (N%2 != 0 and i < (N*N/2)):
-            print('Player 1 Go!')
-            Player1 = win.getMouse()
-            X1_Value = Player1.getX()
-            Y1_Value = Player1.getY()
-            print(X1_Value, Y1_Value)
+        print('Player 1 Go!')
+        Player1 = win.getMouse()
+        X1_Value = Player1.getX()
+        Y1_Value = Player1.getY()
+        print(X1_Value, Y1_Value)
 
-            Move1 = Text(Point(X1_Value, Y1_Value), "X")
-            Move1.setSize(36)
-            Move1.setTextColor(color_1)
-            Move1.draw(win)
-
+        
+        Move1 = None
 # to record where the 'X' is played and insert a 1 in that index within our matrix
-            for b in range(0, N):
-                for a in range(0, N):
-                    if ((100 + 100 * a) < X1_Value < (200 + 100 * a)) and ((100 + 100 * b) < Y1_Value < (200 + 100 * b)):
+        for b in range(0, N):
+            for a in range(0, N):
+                if ((100 + 100 * a) < X1_Value < (200 + 100 * a)) and ((100 + 100 * b) < Y1_Value < (200 + 100 * b)):
+                    if not grid[b][a]:
+                        grid[b][a]
                         grid[b].pop(a)
                         grid[b].insert(a, 1)
                         print(grid)
+                        Move1 = Text(Point(X1_Value, Y1_Value), "X")
+                        Move1.setSize(36)
+                        Move1.setTextColor(color_1)
+                        Move1.draw(win)
+
+
+
+        # if move isn't valid - loop around so they can try again
 
 # to determine if the player 1 won
         # columns
-            for f in range (0, N):
-                col1 = []
-                for e in range (0, N):
-                    if grid[e][f] == 1:
-                        col1.append(1)
-                        print(col1)
-                if col1 == ([1] * N):
-                    print('Player 1 Wins!')
-                    sys.exit('The game is over!')
+        for f in range (0, N):
+            col1 = []
+            for e in range (0, N):
+                if grid[e][f] == 1:
+                    col1.append(1)
+                    print(col1)
+            if col1 == ([1] * N):
+                print('Player 1 Wins!')
+                sys.exit('The game is over!')
 
         # rows
-            for f in range (0, N):
-                row1 = []
-                for e in range (0, N):
-                    if grid[f][e] == 1:
-                        row1.append(1)
-                if row1 == ([1] * N):
-                    print('Player 1 Wins!')
-                    sys.exit('The game is over!')
+        for f in range (0, N):
+            row1 = []
+            for e in range (0, N):
+                if grid[f][e] == 1:
+                    row1.append(1)
+            if row1 == ([1] * N):
+                print('Player 1 Wins!')
+                sys.exit('The game is over!')
 
         # diagonals
-            diag1 = []
-            for g in range(0, N):
-                if grid[g][g] == 1:
-                    diag1.append(1)
-                if diag1 == ([1] * N):
-                    print('Player 1 Wins!')
-                    sys.exit('The game is over!')
+        diag1 = []
+        for g in range(0, N):
+            if grid[g][g] == 1:
+                diag1.append(1)
+            if diag1 == ([1] * N):
+                print('Player 1 Wins!')
+                sys.exit('The game is over!')
 
         # reverse diagonal
-            rev_diag1 = []
-            for g in range(0, N):
-                if grid[g][N - 1 - g] == 1:
-                    rev_diag1.append(1)
-                if rev_diag1 == ([1] * N):
-                    print('Player 1 Wins!')
-                    sys.exit('The game is over!')
+        rev_diag1 = []
+        for g in range(0, N):
+            if grid[g][N - 1 - g] == 1:
+                rev_diag1.append(1)
+            if rev_diag1 == ([1] * N):
+                print('Player 1 Wins!')
+                sys.exit('The game is over!')
 
 
-            if sum(map(sum, grid)) == ((N*N/2)*3)-0.5:
-                break
+        if sum(map(sum, grid)) == ((N*N/2)*3)-0.5:
+            break
 
 
-            print('Player 2 Go!')
-            Player2 = win.getMouse()
-            X2_Value = Player2.getX()
-            Y2_Value = Player2.getY()
-            print(X2_Value, Y2_Value)
+        print('Player 2 Go!')
+        Player2 = win.getMouse()
+        X2_Value = Player2.getX()
+        Y2_Value = Player2.getY()
+        print(X2_Value, Y2_Value)
 
-            Move2 = Text(Point(X2_Value, Y2_Value), "O")
-            Move2.setSize(36)
-            Move2.setTextColor(color_2)
-            Move2.draw(win)
+        Move2 = Text(Point(X2_Value, Y2_Value), "O")
+        Move2.setSize(36)
+        Move2.setTextColor(color_2)
+        Move2.draw(win)
 
 
 
 # to record where the 'O' is played and insert a 2 in that index within our matrix
-            for d in range(0, N):
-                for c in range(0, N):
-                    if ((100 + 100 * c) < X2_Value < (200 + 100 * c)) and ((100 + 100 * d) < Y2_Value < (200 + 100 * d)):
-                        grid[d].pop(c)
-                        grid[d].insert(c, 2)
-                        print(grid)
+        for d in range(0, N):
+            for c in range(0, N):
+                if ((100 + 100 * c) < X2_Value < (200 + 100 * c)) and ((100 + 100 * d) < Y2_Value < (200 + 100 * d)):
+                    grid[d].pop(c)
+                    grid[d].insert(c, 2)
+                    print(grid)
 
 # to determine if the player 2 won
         # columns
-            for f in range (0, N):
-                col2 = []
-                for e in range (0, N):
-                    if grid[e][f] == 2:
-                        col2.append(2)
-                        print(col2)
-                if col2 == ([2] * N):
-                    print('Player 2 Wins!')
-                    sys.exit('The game is over!')
+        for f in range (0, N):
+            col2 = []
+            for e in range (0, N):
+                if grid[e][f] == 2:
+                    col2.append(2)
+                    print(col2)
+            if col2 == ([2] * N):
+                print('Player 2 Wins!')
+                sys.exit('The game is over!')
 
         # rows
-            for f in range (0, N):
-                row2 = []
-                for e in range (0, N):
-                    if grid[f][e] == 2:
-                        row2.append(2)
-                if row2 == ([2] * N):
-                    print('Player 2 Wins!')
-                    sys.exit('The game is over!')
+        for f in range (0, N):
+            row2 = []
+            for e in range (0, N):
+                if grid[f][e] == 2:
+                    row2.append(2)
+            if row2 == ([2] * N):
+                print('Player 2 Wins!')
+                sys.exit('The game is over!')
 
 
         # diagonals
-            diag2 = []
-            for g in range(0, N):
-                if grid[g][g] == 2:
-                    diag2.append(2)
-                if diag2 == ([2] * N):
-                    print('Player 2 Wins!')
-                    sys.exit('The game is over!')
+        diag2 = []
+        for g in range(0, N):
+            if grid[g][g] == 2:
+                diag2.append(2)
+            if diag2 == ([2] * N):
+                print('Player 2 Wins!')
+                sys.exit('The game is over!')
 
         # reverse diagonal
-            rev_diag2 = []
-            for g in range(0, N):
-                if grid[g][N - 1 - g] == 2:
+        rev_diag2 = []
+        for g in range(0, N):
+            if grid[g][N - 1 - g] == 2:
                     rev_diag2.append(2)
-                if rev_diag2 == ([2] * N):
-                    print('Player 2 Wins!')
-                    sys.exit('The game is over!')
+            if rev_diag2 == ([2] * N):
+                print('Player 2 Wins!')
+                sys.exit('The game is over!')
 
-            if sum(map(sum, grid)) == ((N*N/2)*3):
-                break
+        if sum(map(sum, grid)) == ((N*N/2)*3):
+            break
 
     print("Oops, it's a tie...")
     sys.exit('This game is over!')
